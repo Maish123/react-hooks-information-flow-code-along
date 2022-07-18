8e7a7bca7d98ba07dd6e3b57fc0a346d98364bf9
Denis Maina <Denis.maina@student.moringaschool.com>
2022-07-18 11:08:19 +0300

    initial commit

--- src/Parent.js
+++ src/Parent.js
@@ -4,12 +4,21 @@ import Child from "./Child";
 
 function Parent() {
   const randomColor = getRandomColor();
-  const [color, setColor] = useState(randomColor);
+  const [color, setColor] = useState(randomColor);//initial value of color state of the parent div
+  const[childrenColor,setChildrenColor]= useState("#FFF")
+
+  function handleChangeColor(newChildColor){
+    console.log(newChildColor);
+    const newRandomColor = getRandomColor();
+    setColor(newRandomColor) // this handles the color randomness of the parent div when the child is clicked
+    setChildrenColor(newChildColor)//this handles the color randomness of the children components
+  } 
+
 
   return (
     <div className="parent" style={{ backgroundColor: color }}>
-      <Child />
-      <Child />
+      <Child color={childrenColor} onChangeColor={handleChangeColor}/>
+      <Child color={childrenColor} onChangeColor={handleChangeColor}/>
     </div>
   );
 }
